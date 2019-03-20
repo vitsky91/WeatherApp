177ba92b60ea07381e21fce60ff5bfa8e8fd363e
Vitalii Sidorskyi <vitalii.syborskyi@xyrality.com>
2019-03-20 10:15:00 +0100

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Vitalii Sydorskyi on 3/20/19.
+//  Copyright © 2019 Vitalii Sydorskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
