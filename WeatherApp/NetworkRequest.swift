5a0761c6aad0845045ec938f15993b0153b920b8
Vitalii Sidorskyi <vitalii.syborskyi@xyrality.com>
2019-03-20 16:10:08 +0100

    WIP: UI for app

---
+++ WeatherApp/NetworkRequest.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkRequest.swift
+//  WeatherApp
+//
+//  Created by Vitalii Sydorskyi on 3/20/19.
+//  Copyright © 2019 Vitalii Sydorskyi. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class NetworkRequest {
+    
+    let city = "Zaporizhzhya"
+    let keyAPI = "213a683e43f9c04006135217e02dda1b"
+    
+    func parser() {
+        let openweatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=\(city)&appid=\(keyAPI)"
+        
+        Alamofire.request(openweatherURL).responseJSON { (response) in
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            response.result.ifSuccess {
+                <#code#>
+            }
+            
+        }
+    }
+    
+}
